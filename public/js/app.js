e8c43835e08e819c298e61c88e92a2818d44a0d9
compaq610 <shahrozali283@gmail.com>
2019-11-05 09:04:21 +0500

    remove unnecessary console.log call

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,3 @@
-console.log("Client side data file loaded!")
-
 const WeatherForm = document.querySelector("form")
 const search = document.querySelector("input")
 const messageone = document.querySelector("#message-1")
